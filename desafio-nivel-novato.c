7ff3b9fa7010c0972e1db9a1d0ae461eadfca68c
Henrique Chagas <60076841+Crocomire@users.noreply.github.com>
2025-03-05 12:42:08 +0000

    Criação de um novo arquivo especifico para o nível novato do desafio

---
+++ desafio-nivel-novato.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Desafio Super Trunfo - Países
+// Tema 1 - Cadastro das Cartas
+// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
+// Siga os comentários para implementar cada parte do desafio.
+//Teste larissa
+
+int main() {
+    //nome do estado
+    char estado01, estado02;
+    //nome da cidade
+    char cidade01, cidade02;
+    //tamanho da população
+    int populacao01, populacao02;
+    //km² da cidade, seu tamanho
+    float area01, area02;
+    //Produto Interno Bruto
+    float pib01, pib02;
+    //Pontos Turisticos
+    int pturistico01, pturistico02;
+
+    
+    
+
+    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    
+    // Cadastro das Cartas:
+    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
+    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    
+    // Exibição dos Dados das Cartas:
+    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
+    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    return 0;
+}
